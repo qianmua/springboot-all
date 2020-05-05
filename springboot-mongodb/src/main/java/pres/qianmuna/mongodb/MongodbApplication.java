1157d06d63df6676f0f9ad17576a0f843d584bbe
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-05-05 16:55:04 +0800

    springboot redis

---
+++ springboot-mongodb/src/main/java/pres/qianmuna/mongodb/MongodbApplication.java
@@ -0,0 +1,21 @@
+package pres.qianmuna.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author HJC
+ * @version 1.0
+ * 谦谦君子 卑以自牧也
+ * @date 2020/5/5
+ * @time 16:49
+ */
+@SpringBootApplication
+public class MongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbApplication.class,args);
+    }
+}
