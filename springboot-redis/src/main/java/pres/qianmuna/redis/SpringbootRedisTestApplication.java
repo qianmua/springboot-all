cb8d0c1f404a24207e459c99943322fb4413e0ad
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-05-13 22:57:19 +0800

    redis

--- springboot-redis/src/main/java/pres/qianmuna/redis/SpringbootRedisTestApplication.java
+++ springboot-redis/src/main/java/pres/qianmuna/redis/SpringbootRedisTestApplication.java
@@ -15,6 +15,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootRedisTestApplication {
 
+    /**
+     * jvm锁 与 分布式锁
+     *
+     * jvm 是 在本地自己的jvm里面拿锁
+     * 分布式锁 就是 我们 的项目程序 向redis去拿锁
+     * 两个锁的地方不一样
+     *
+     * redis是基于单线程的所以可以实现分布式锁
+     * 同一时间是只有一个响应
+     *
+     * 可以 使用setnx 实现 （数据库有value 则 返回no ， 没有 就创建  相当于加锁 ）
+     *
+     * 解锁 ： 使用守护线程 去监听过期时间，当这个资源被释放时，守护线程也会挂掉，redis上面就会过期就释放了锁
+     *
+     * */
+
     /*
     * springboot 2.x 之后 jedis 被替换成lettuce
     * jedis 采用直连，多个线程操作不安全 ， 避免 就使用jedis pool 线程池 BIO
@@ -23,6 +39,7 @@ public class SpringbootRedisTestApplication {
     *
     * springboot 所有配置类都有一个自动配置文类 spring.factories
     * 绑定一个properties 配置文件
+    *
     *
     * */
 
