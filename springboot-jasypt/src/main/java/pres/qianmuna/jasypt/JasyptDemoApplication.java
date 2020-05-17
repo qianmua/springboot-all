300d3df731fbbd34692712a5fc188c0f30d0a865
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-05-17 12:42:41 +0800

    jasypt

---
+++ springboot-jasypt/src/main/java/pres/qianmuna/jasypt/JasyptDemoApplication.java
@@ -0,0 +1,23 @@
+package pres.qianmuna.jasypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author HJC
+ * @version 1.0
+ * 谦谦君子 卑以自牧也
+ * @date 2020/5/17
+ * @time 12:32
+ */
+@SpringBootApplication
+public class JasyptDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JasyptDemoApplication.class,args);
+
+
+    }
+}
---
+++ springboot-jasypt/src/main/java/pres/qianmuna/jasypt/controller/ProviderController.java
@@ -0,0 +1,42 @@
+package pres.qianmuna.jasypt.controller;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author HJC
+ * @version 1.0
+ * 谦谦君子 卑以自牧也
+ * @date 2020/5/17
+ * @time 12:36
+ */
+@RestController
+public class ProviderController {
+
+    private final StringEncryptor stringEncryptor;
+
+    public ProviderController(StringEncryptor stringEncryptor) {
+        this.stringEncryptor = stringEncryptor;
+    }
+
+    @Value("${datatype}")
+    String value;
+
+    @GetMapping("/g/{v}")
+    public String get(@PathVariable String v){
+        String str = stringEncryptor.encrypt(v);
+        System.out.println(str);
+        return str;
+    }
+
+    @GetMapping("/get")
+    public String get2(){
+        return value;
+    }
+}
