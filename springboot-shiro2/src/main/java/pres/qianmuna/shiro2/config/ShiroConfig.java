6e061b3a3525f98339db95f2172b2b21565aa1fb
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-06-17 14:11:31 +0800

    shiro2 rote

--- springboot-shiro2/src/main/java/pres/qianmuna/shiro2/config/ShiroConfig.java
+++ springboot-shiro2/src/main/java/pres/qianmuna/shiro2/config/ShiroConfig.java
@@ -1,6 +1,14 @@
 package pres.qianmuna.shiro2.config;
 
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import pres.qianmuna.shiro2.handler.ShiroRealm;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * @author HJC
@@ -11,5 +19,77 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class ShiroConfig {
+
+    /*
+    *
+    * shiro
+    *
+    * 认证 授权
+    *
+    * 安全管理器 realm
+    * 缓存管理器 ehcache             redis
+    * 会话管理器 分布式会话管理       redis
+    *
+    *
+    * shiro 启动器 需要 在resources 下面 多一个 shiro.ini 配置 才能去加载 // 配置 ini
+    * 初始化
+    * // 扩展非常麻烦
+    *
+    *
+    *
+    * */
+
+
+    /**
+     * 自定义 realm
+     * @return
+     */
+    @Bean
+    public ShiroRealm shiroRealm(){
+        return new ShiroRealm();
+    }
+
+
+    /**
+     * 配置安全管理器
+     * @return
+     */
+    @Bean
+    public SecurityManager securityManager(){
+        DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
+
+        manager.setRealm(shiroRealm());
+        return manager;
+    }
+
+    /**
+     * 过滤规则
+     * @return
+     */
+    @Bean
+    public ShiroFilterFactoryBean shiroFilterFactoryBean(){
+        ShiroFilterFactoryBean bean = new ShiroFilterFactoryBean();
+
+        bean.setSecurityManager(securityManager());
+
+        bean.setLoginUrl("/login");
+
+        Map<String, String> hashMap = new LinkedHashMap<>();
+
+
+        hashMap.put("/login" , "anno");
+        hashMap.put("/css/**" , "anno");
+        hashMap.put("/img/**" , "anno");
+        hashMap.put("/js/**" , "anno");
+        hashMap.put("/lib/**" , "anno");
+        hashMap.put("/**" , "user");
+
+        // 过滤规则
+        bean.setFilterChainDefinitionMap(hashMap);
+
+        return bean;
+    }
+
+
 
 }
---
+++ springboot-shiro2/src/main/java/pres/qianmuna/shiro2/handler/ShiroRealm.java
@@ -0,0 +1,70 @@
+package pres.qianmuna.shiro2.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * @author HJC
+ * @version 1.0
+ * 谦谦君子 卑以自牧也
+ * @date 2020/6/17  13:15
+ * @description :
+ */
+@Slf4j
+public class ShiroRealm extends AuthorizingRealm {
+    /**
+     * 授权
+     * @param principalCollection
+     * @return
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+
+        // 授权检查
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+
+        // 添加 角色
+        info.addRole("admin");
+        // 添加权限
+        info.addStringPermission("cms:news:view");
+        info.addStringPermission("cms:news:list");
+
+        log.info("rote success...");
+
+        return info;
+    }
+
+    /**
+     * 认证
+     * @param authenticationToken
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        // 得到token
+        UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
+
+        String username = token.getUsername();
+
+        // 思考 为什么 要 new
+        String s = new String(token.getPassword());
+
+        // 查找 用户/////
+        ///==========================///
+        ///==========================///
+        ///==========================///
+
+        if ( !"admin".equals(username) || !"123456".equals(s))
+            throw new RuntimeException("username password is error");
+
+        // 创建 信息认证
+        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(username, s, super.getName());
+
+        return info;
+    }
+}
