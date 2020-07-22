4bb1e13210184a7b26989993ce6d2bcedea11188
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-07-22 17:10:02 +0800

    spring rpc api , client base

---
+++ springboot-netty-rpc2/rpc-api/src/main/java/pres/qianmuna/rpc/service/TestService.java
@@ -0,0 +1,13 @@
+package pres.qianmuna.rpc.service;
+
+/**
+ * @author HJC
+ * @version 1.0
+ * 谦谦君子 卑以自牧也
+ * @date 2020/7/22  17:08
+ * @description :
+ */
+public interface TestService {
+
+    String sayHello(String hello);
+}
---
+++ springboot-netty-rpc2/rpc-client/src/main/java/pres/qianmuna/rpc/consumer/RpcConsumer.java
@@ -0,0 +1,13 @@
+package pres.qianmuna.rpc.consumer;
+
+/**
+ * @author HJC
+ * @version 1.0
+ * 谦谦君子 卑以自牧也
+ * @date 2020/7/22  17:06
+ * @description :
+ */
+public class RpcConsumer {
+
+
+}
