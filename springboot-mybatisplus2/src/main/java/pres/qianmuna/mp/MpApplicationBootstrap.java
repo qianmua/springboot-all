cc6d8528c1229db026b85e470b07143e861fc2b7
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-09-02 20:18:59 +0800

    mp + swagger

---
+++ springboot-mybatisplus2/src/main/java/pres/qianmuna/mp/MpApplicationBootstrap.java
@@ -0,0 +1,19 @@
+package pres.qianmuna.mp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HJC
+ * @version 1.0
+ * 谦谦君子 卑以自牧也
+ * @date 2020/9/2  20:11
+ * @description :
+ */
+@SpringBootApplication
+public class MpApplicationBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpApplicationBootstrap.class , args);
+    }
+}
