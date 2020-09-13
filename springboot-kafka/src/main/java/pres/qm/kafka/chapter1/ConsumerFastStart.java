f6b873dbcee885386f0a96171805c5ff58a0fc5b
qianmua <49187025+qianmua@users.noreply.github.com>
2020-09-13 22:23:31 +0800

    kafka

---
+++ springboot-kafka/src/main/java/pres/qm/kafka/chapter1/ConsumerFastStart.java
@@ -0,0 +1,44 @@
+package pres.qm.kafka.chapter1;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+/**
+ * @author HJC
+ * @version 1.0
+ * 谦谦君子 卑以自牧也
+ * @date 2020/9/13  12:56
+ * @description :
+ */
+public class ConsumerFastStart {
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        // 序列化器
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        // 值序列化器
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG , "127.0.0.1:9092");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG , "group.qm");
+
+        // 消息订阅
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Collections.singletonList("qm_kafka"));
+
+        while (true){
+            // 消息 订阅
+            ConsumerRecords<String, String> poll = consumer.poll(Duration.ofMillis(1000L));
+            poll.forEach(v1 -> System.out.println(v1.value()));
+
+        }
+    }
+}
--- springboot-kafka/src/main/java/pres/qm/kafka/chapter1/ProductFastStart.java
+++ springboot-kafka/src/main/java/pres/qm/kafka/chapter1/ProductFastStart.java
@@ -1,6 +1,8 @@
 package pres.qm.kafka.chapter1;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
@@ -17,21 +19,27 @@ public class ProductFastStart {
     public static void main(String[] args) {
         Properties properties = new Properties();
         // 序列化器
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG ,"org.apache.kafka.common.serialization.Serializer");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG ,StringSerializer.class.getName());
+//        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG ,"org.apache.kafka.common.serialization.Serializer");
 
         // 重试
         properties.put(ProducerConfig.RETRIES_CONFIG ,  10);
 
         // 值序列化器
-        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG , "org.apache.kafka.common.serialization.Serializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG , StringSerializer.class.getName());
 
         // 集群地址
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG , "localhost:9092");
 
         // product
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
+        // 消息发送
+        ProducerRecord<String, String> record = new ProducerRecord<>("qm", "test_msg", "hello qm kafka");
 
+        producer.send(record);
 
+        producer.close();
 
     }
 }
