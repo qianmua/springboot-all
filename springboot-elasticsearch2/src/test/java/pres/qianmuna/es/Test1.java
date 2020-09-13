2c09ec83cb5187c9fa3b75b7e4e02f10697608e2
qianmua <49187025+qianmua@users.noreply.github.com>
2020-09-13 12:24:23 +0800

    es 完结

--- springboot-elasticsearch2/src/test/java/pres/qianmuna/es/Test1.java
+++ springboot-elasticsearch2/src/test/java/pres/qianmuna/es/Test1.java
@@ -10,6 +10,7 @@ import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -17,13 +18,17 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.common.text.Text;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.MatchAllQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -32,6 +37,7 @@ import pres.qianmuna.es.entity.Member;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -167,19 +173,86 @@ public class Test1 {
     }
 
     @Test
-    public void testSearch(){
+    public void testSearch() throws IOException {
         SearchRequest request = new SearchRequest("qm_index");
 
         // 构建条件
         // 构造器 构造条件
         // 构造器模式
         SearchSourceBuilder builder = new SearchSourceBuilder();
+        // highlight
+        // xxx
         TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery("name", "a");
         MatchAllQueryBuilder matchAllQuery = QueryBuilders.matchAllQuery();
+
         builder.query(termQueryBuilder);
         builder.from(0);
         builder.size(20);
         builder.timeout(new TimeValue(60 , TimeUnit.SECONDS));
+
+        request.source(builder);
+
+        SearchResponse response = restHighLevelClient.search(request, RequestOptions.DEFAULT);
+
+        System.out.println(response.toString());
+        System.out.println(JSON.toJSONString(response.getHits()));
+        response.getHits().forEach(v1 -> System.out.println(v1.getSourceAsMap()));
+
+    }
+
+    @Test
+    public void testSearchHighlight() throws IOException {
+        String keyWord = "";
+        int current = 0;
+        int limit = 10;
+
+        SearchRequest request = new SearchRequest("qm_index");
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+
+        builder.from(current);
+        builder.size(limit);
+
+        TermQueryBuilder query = QueryBuilders.termQuery("title", keyWord);
+        builder.query(query);
+
+        builder.timeout(new TimeValue(20 , TimeUnit.SECONDS));
+
+        // 高亮
+        HighlightBuilder builder1 = new HighlightBuilder();
+        builder1.field("title");
+        builder1.preTags("<span style:'color:red'>");
+        builder1.postTags("</span>");
+        // 多个字段高亮
+//        builder1.requireFieldMatch(true);
+        builder.highlighter(builder1);
+
+        request.source(builder);
+        SearchResponse response = restHighLevelClient.search(request, RequestOptions.DEFAULT);
+        ArrayList<Map<String, Object>> list = new ArrayList<>();
+
+        //解析高亮
+        /*response.getHits().forEach(v1 -> {
+
+        });*/
+        for (SearchHit hit : response.getHits().getHits()) {
+            // 高亮字段
+            Map<String, HighlightField> map = hit.getHighlightFields();
+            HighlightField title = map.get("title");
+            // 结果字段
+            Map<String, Object> sourceAsMap = hit.getSourceAsMap();
+
+            if (title != null){
+                Text[] texts = title.fragments();
+                StringBuilder titles = new StringBuilder();
+                // 替换
+                for (Text text : texts) {
+                    titles.append(text);
+                }
+                sourceAsMap.put("title" , titles);
+            }
+            // result
+            list.add(sourceAsMap);
+        }
 
     }
 
