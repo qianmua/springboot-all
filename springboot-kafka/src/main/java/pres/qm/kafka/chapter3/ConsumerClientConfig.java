12b1702bbecde6bdedfb78d6a120cd806f5c7020
qianmua <49187025+qianmua@users.noreply.github.com>
2020-09-15 11:19:20 +0800

    spring + mybatis plus

---
+++ springboot-kafka/src/main/java/pres/qm/kafka/chapter3/ConsumerClientConfig.java
@@ -0,0 +1,13 @@
+package pres.qm.kafka.chapter3;
+
+/**
+ * @author HJC
+ * @version 1.0
+ * 谦谦君子 卑以自牧也
+ * @date 2020/9/14  23:16
+ * @description : 指定位移消费
+ */
+public class ConsumerClientConfig {
+
+
+}
