144790230f170f9b6bc72169ade91b733db34763
qianmua <49187025+qianmua@users.noreply.github.com>
2020-10-20 01:09:06 +0800

    aqs rest ♥

---
+++ springboot-aqs/src/main/java/pres/qm/aqs/AQSApplicationBootstrap.java
@@ -0,0 +1,19 @@
+package pres.qm.aqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HJC
+ * @version 1.0
+ * 谦谦君子 卑以自牧也
+ * @date 2020/10/20  1:06
+ * @description :
+ */
+@SpringBootApplication
+public class AQSApplicationBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AQSApplicationBootstrap.class,args);
+    }
+}
---
+++ springboot-aqs/src/main/java/pres/qm/aqs/api/AQSController.java
@@ -0,0 +1,18 @@
+package pres.qm.aqs.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author HJC
+ * @version 1.0
+ * 谦谦君子 卑以自牧也
+ * @date 2020/10/20  1:07
+ * @description :
+ */
+@RestController
+@RequestMapping("/shop")
+public class AQSController {
+
+
+}
